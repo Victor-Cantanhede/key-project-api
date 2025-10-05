9a5b0080508043c95b087f237bbb4da799cae96c
Victor Cantanhede <victorcantanhedblk@gmail.com>
2025-10-05 12:21:41 -0300

    dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './_modules/user/user.module';
 import { KeysModule } from './_modules/keys/keys.module';
 import { AuthModule } from './_modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, KeysModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${ process.env.NODE_ENV || 'development' }`
+    }),
+    UserModule,
+    KeysModule,
+    AuthModule
+  ],
   controllers: [],
   providers: []
 })
